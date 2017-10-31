84b2a05d9b52618e1aac2671f54df691d035324c
Ellie Hamilton <elliehamilton@Ellies-MacBook-Pro.local>
2017-10-31 14:24:43 +0000

    Remove for loop and refactor var to let

--- 12_exercise.js
+++ 12_exercise.js
@@ -21,10 +21,7 @@ function isInListOfFilters(listOfFilters, filter) {
 }
 
 function filterApplicants(applicants, filters) {
-  var filteredApplicants = [];
-  var applicantsLength = applicants.length;
-  var filterLength = filters.length;
-  var hasAttributes;
+  let filteredApplicants = [];
 
   if (!filters.length) return applicants;
 
@@ -40,42 +37,13 @@ function filterApplicants(applicants, filters) {
   }
   else {
     filteredApplicants = applicants.filter(function(applicant){
-      for (var k = filterLength; k--; ) {
-        if (!isInListOfFilters(applicant.attributes, filters[k])) return false;
-      }
+      let list = filters.filter(function(filter){
+        return isInListOfFilters(applicant.attributes, filter);
+      });
+      if (list.length !== filters.length) return false;
       return true;
     });
   }
-
-
-  // for (var i = applicantsLength; i--; ) {
-  //   hasAttributes = applicants[i].attributes && applicants[i].attributes.length > 0;
-
-  //   if (hasAttributes) {
-  //     for (var k = filterLength; k--; ) {
-  //       var hasFilter = false;
-  //       for (var j = applicants[i].attributes.length; j--; ) {
-  //         if (!availableImmediately && !freshGrad) {
-  //           // if (filters[k].indexOf(applicants[i].attributes[j]) !== -1) {
-  //           if(isInListOfFilters(applicants[i].attributes, filters[k])){
-  //             hasFilter = true;
-  //           }
-  //         } else if (
-  //           availableImmediately &&
-  //           applicants[i].attributes[j] === 'AVAILABLE_IMMEDIATELY'
-  //         ) {
-  //           hasFilter = true;
-  //         } else if (freshGrad && applicants[i].attributes[j] === 'FRESH_GRAD') {
-  //           hasFilter = true;
-  //         }
-  //       }
-  //       hasAttributes = hasAttributes && hasFilter;
-  //     }
-  //   }
-  //   if (hasAttributes) {
-  //     filterApplicants.unshift(applicants[i]);
-  //   }
-  // }
   return filteredApplicants;
 }
 
