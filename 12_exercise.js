9a3f16bcf8e38e804b8170eec33044e5b16612ab
Ellie Hamilton <elliehamilton@Ellies-MacBook-Pro.local>
2017-10-31 15:30:00 +0000

    Move filter to the outside of all of the statements

--- 12_exercise.js
+++ 12_exercise.js
@@ -1,32 +1,20 @@
-let filterApplicantsByPriorityAttribute = (applicants, attribute) => {
-  return applicants.filter((applicant) => {
-    return applicant.attributes.includes(attribute);
-  });
-}
-
-let filterApplicantsByAllAttributes = (applicants, attributes) => {
-  return applicants.filter((applicant) => {
-    return attributes.every((attribute) => {
-      return applicant.attributes.includes(attribute);
-    });
-  });
-}
-
 let filterApplicantsByAttributes = (applicants, attributesToFilterBy) => {
   const AVAILABLE_IMMEDIATELY = 'AVAILABLE_IMMEDIATELY';
   const FRESH_GRAD = 'FRESH_GRAD';
 
   if (!attributesToFilterBy.length) return applicants;
 
-  if (attributesToFilterBy.includes(AVAILABLE_IMMEDIATELY)) {
-    return filterApplicantsByPriorityAttribute(applicants, AVAILABLE_IMMEDIATELY);
-  } 
-  else if (attributesToFilterBy.includes(FRESH_GRAD)) {
-    return filterApplicantsByPriorityAttribute(applicants, FRESH_GRAD);
-  }
-  else {
-    return filterApplicantsByAllAttributes(applicants, attributesToFilterBy);
-  }
+  return applicants.filter((applicant) => {
+    if (attributesToFilterBy.includes(AVAILABLE_IMMEDIATELY)) {
+      return applicant.attributes.includes(AVAILABLE_IMMEDIATELY);
+    } 
+    if (attributesToFilterBy.includes(FRESH_GRAD)) {
+      return applicant.attributes.includes(FRESH_GRAD);
+    }
+    return attributesToFilterBy.every((attribute) => {
+      return applicant.attributes.includes(attribute);
+      });
+  });
 }
 
 module.exports = filterApplicantsByAttributes;
