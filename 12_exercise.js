589544a4272f1b9db358e3175c136862dc3083f8
Ellie Hamilton <elliehamilton@Ellies-MacBook-Pro.local>
2017-10-31 15:09:52 +0000

    Abstract priority attribute into separate function

--- 12_exercise.js
+++ 12_exercise.js
@@ -1,39 +1,22 @@
-/**
- *  AVAILABLE FILTERS:
- *  ["AVAILABLE_IMMEDIATELY", "FRESH_GRAD", "JUNIOR", "JAVASCRIPT", "PHP", "AWS", "REACT", "JAVA"]
- *   
- *  "AVAILABLE_IMMEDIATELY" and "FRESH_GRAD" will override all the other filters if specified
- * 
- *  if "AVAILABLE_IMMEDIATELY" and "FRESH_GRAD" are both specified as filter, "FRESH_GRAD" will be ignored
- * 
- * 
- *  Exercise: refactor this code
- *  - take care of naming variables
- *  - get rid of the 'for loops'
- *  - move it to modern JS!
- *  - oh, there are missing tests/scenario
- *  
- *   happy refactory :)
- */
-
-var filterApplicantsByAttributes = (applicants, attributesToFilterBy) => {
+let filterApplicantsByPriorityAttribute = (applicants, attribute) => {
+  return applicants.filter((applicant) => {
+    return applicant.attributes.includes(attribute);
+  });
+}
+let filterApplicantsByAttributes = (applicants, attributesToFilterBy) => {
   const AVAILABLE_IMMEDIATELY = 'AVAILABLE_IMMEDIATELY';
   const FRESH_GRAD = 'FRESH_GRAD';
 
   if (!attributesToFilterBy.length) return applicants;
 
   if (attributesToFilterBy.includes(AVAILABLE_IMMEDIATELY)) {
-    return filteredApplicants = applicants.filter((applicant) => {
-      return applicant.attributes.includes(AVAILABLE_IMMEDIATELY);
-    });
+    return filterApplicantsByPriorityAttribute(applicants, AVAILABLE_IMMEDIATELY);
   } 
   else if (attributesToFilterBy.includes(FRESH_GRAD)) {
-    return filteredApplicants = applicants.filter((applicant) => {
-      return applicant.attributes.includes(FRESH_GRAD);
-    });
+    return filterApplicantsByPriorityAttribute(applicants, FRESH_GRAD);
   }
   else {
-    return filteredApplicants = applicants.filter((applicant) => {
+    return applicants.filter((applicant) => {
       return attributesToFilterBy.every((attribute) => {
         return applicant.attributes.includes(attribute);
       });
